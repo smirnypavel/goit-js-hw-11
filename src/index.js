5b348832e05bb54eec912d5e22d63b3482db5cd0
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2022-12-08 11:47:27 +0200

    almost done
    almost done

--- src/index.js
+++ src/index.js
@@ -5,23 +5,47 @@ import { fetchForm } from './js/fetch'
 import {createMarkup} from './js/markap'
 
 const gallery = document.querySelector('.gallery');
-const form= document.querySelector('.search-form');
+const form = document.querySelector('.search-form');
+const guard = document.querySelector('.js_guard');
+const options = {
+    root: null,
+    rootMargin: '200px',
+    threshold: 1.0
+}
+const observer = new IntersectionObserver(onLoad, options);
+let page =1;
 
+function onLoad(entries, observer){
+    entries.forEach(element => {
+        page+=1;
+        if(element.isIntersecting){
+            console.log('БАЧУ');
+            fetchForm(page).then(data =>
+                gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits))
+                )
+        }
+    });
+    console.log(entries);
+}
 
 form.addEventListener('submit', searchOn);
 
 function searchOn(evt){
     evt.preventDefault()
-    console.dir(evt.currentTarget);
+    // console.dir(evt.currentTarget);
     const {searchQuery : {value }} = evt.currentTarget.elements;
-    console.log(value);
+    // console.log(value);
     if(!value){
+        clearMarkAp();
         Notiflix.Notify.failure('Please, enter a search term!!!');
         return
     }
     fetchForm(value).then(data => {
-            console.log(data.hits);
+            console.log(data);
+            Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
+            clearMarkAp();
             gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits))
+            observer.observe(guard)
         });
 }
 
@@ -35,10 +59,12 @@ function searchOn(evt){
 // });
 // fetchForm()
 
-let lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: `alt`,
-    captionDelay: 250,
-    fadeSpeed: 100,
-});
+// let lightbox = new SimpleLightbox('.gallery a', {
+//     captionsData: `alt`,
+//     captionDelay: 250,
+//     fadeSpeed: 100,
+// });
 
-
+function clearMarkAp(){
+    gallery.innerHTML = '';
+}
